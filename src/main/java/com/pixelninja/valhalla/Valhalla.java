2b79d9594918562aa22e2c8ed159ec38fb5fb03f
PixelNinja5467 <81436718+PixelNinja5467@users.noreply.github.com>
2022-01-06 10:14:59 +0000

    Initial commit

---
+++ src/main/java/com/pixelninja/valhalla/Valhalla.java
@@ -0,0 +1,12 @@
+package com.pixelninja.valhalla;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Valhalla implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+    
+}
